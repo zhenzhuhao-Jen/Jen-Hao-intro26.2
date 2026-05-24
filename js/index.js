60d208c28c156935f2c696f2a8ec01d0bbc1669c
zhenzhuhao-Jen <zhenzhuhao@gmail.com>
2026-05-24 14:09:24 -0400

    Lesson-7 changes made

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const footerElement = document.createElement('footer');
 const bodyElement = document.querySelector('body')
 bodyElement.appendChild(footerElement);
 
+
 const today = new Date();
 
 let thisYear = today.getFullYear();
@@ -11,19 +12,18 @@ const footer = document.querySelector('footer');
 
 const copyright = document.createElement('p');
 
-copyright.innerHTML = "Zhenzhu Hao " + thisYear;
+copyright.innerHTML = "Zhenzhu Hao " + thisYear + "&copy";
 
 footer.appendChild(copyright);
+
+
 
 const skills = ["JavaScript", "HTML", "CSS", "GitHub"];
 const skillsSection = document.querySelector('#skills');
 const skillsList = skillsSection.querySelector('ul');
 
-skills.forEach((skill)=>{
-
-    const li = document.createElement('li');
-   
-     li.innerHTML = skill;
-     
-     skillsList.appendChild(li);
-});
+for(let i = 0; i < skills.length; i++){
+    const skill = document.createElement('li');
+    skill.innerHTML = skills[i];
+    skillsList.appendChild(skill);
+}
