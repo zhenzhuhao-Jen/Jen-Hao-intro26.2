6c0e0262fb6af179bf599f208ac46a5a47737ee4
zhenzhuhao-Jen <zhenzhuhao@gmail.com>
2026-05-26 23:08:28 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -26,4 +26,38 @@ for(let i = 0; i < skills.length; i++){
     const skill = document.createElement('li');
     skill.innerHTML = skills[i];
     skillsList.appendChild(skill);
-}
+};
+
+
+const messageForm = document.querySelector('form[name]');//problems?
+
+messageForm.addEventListener("submit",function(event){
+    event.preventDefault();
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+    console.log(usersName, usersEmail, usersMessage);
+
+
+    messageSection = document.querySelector('#messages');
+    messageList = messageSection.querySelector('ul');
+    newMessage = document.createElement('li');
+    newMessage.innerHTML += 
+    `<a href ="mailto:${usersEmail}">${usersName}</a>
+    <span>${usersMessage}</span>`; 
+    
+
+    const removeButton = document.createElement('button');
+    removeButton.innerHTML = "remove";
+    removeButton.type = "button";
+    removeButton.addEventListener("click", function(){
+        const entry = removeButton.parentNode;
+        entry.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+
+    messageForm.reset();
+});
